1763569e5fbe92b4ccab6c330e0f2364726262e0
udita@easypiecy.com <udita@easypiecy.com>
2021-12-22 11:46:44 +0100

    added address in Employee table

--- EFCoreMysql/Migrations/20211222104451_Employee_addcolumn.Designer.cs
+++ EFCoreMysql/Migrations/20211222104451_Employee_addcolumn.Designer.cs
@@ -2,14 +2,16 @@
 using EFCoreMysql.DBContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EFCoreMysql.Migrations
 {
     [DbContext(typeof(MyDBContext))]
-    partial class MyDBContextModelSnapshot : ModelSnapshot
+    [Migration("20211222104451_Employee_addcolumn")]
+    partial class Employee_addcolumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
